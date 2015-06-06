e4f7100d22b77ba97bee36034fe325cee6e73520
Dimitri-Miles <dimitri.miles@vonagebusiness.com>
2015-06-06 19:48:31 -0400

    edited the gulpfile to include comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,21 @@ var gulputil = require('gulp-util');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 
+//Runs JSHINT against all JavaScript files in the approved QA repository
 gulp.task('jshint', function(){
     gulp.src('QA/Todd/*.js')
     .pipe(jshint()) 
     .pipe(jshint.reporter('default', { verbose: true }));
 });
 
+//Concats all of the JavaScript files approved by QA into a single file
 gulp.task('concat-js', function(){
     gulp.src('QA/Todd/*.js')
     .pipe(concat("production.js"))
     .pipe(gulp.dest('QA/Todd'))
 });
 
+//Deploys the compiled file to the production environment
 gulp.task('deploy', function(){
     gulp.src('QA/Todd/production.js')
     .pipe(gulp.dest('production'))
